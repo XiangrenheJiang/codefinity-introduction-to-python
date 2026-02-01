96e116b44fc08b09d606097912e169530c97a49f
XiangrenheJiang <>
2026-02-01 20:32:13 +0000

    Nested loops

---
+++ loops/nested_loops/main.py
@@ -0,0 +1,10 @@
+produce = ["Tomatoes", "Lettuce"]
+dairy = ["Milk", "Cheese"]
+groceries = []
+groceries.append(produce)
+groceries.append(dairy)
+print(groceries)
+
+for section in groceries:
+    for item in section:
+        print(f"Item name: {item}")
